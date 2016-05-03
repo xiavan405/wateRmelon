c14510f90784fc78a2b3c6fc1d0e0585307f3606
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-05-03 21:26:31 +0000

    Creating branch for BioC 3.3 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_3/madman/Rpacks/wateRmelon@117080 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/y_minfi.R
@@ -0,0 +1,613 @@
+# methods for MethylSet (minfi)
+
+#setClass("MethylSet",
+# representation("VIRTUAL",
+# description = "character"))
+
+#setClass("RGChannelSet",
+# representation("VIRTUAL",
+# description = "character"))
+
+
+# betaqn <- function (bn){ 
+setMethod(
+   f= "betaqn",
+   signature(bn="MethylSet"),
+   definition=function(bn){
+      object <- bn
+      betaqn (
+         bn = getBeta(object) 
+      ) 
+   }
+)
+
+setMethod(
+   f= "betaqn",
+   signature(bn="RGChannelSet"),
+   definition=function(bn){
+      object <- bn
+      betaqn (
+      bn = getBeta(object) 
+      ) 
+   }
+)
+
+# naten <- function (mn, un, fudge=100 ) {
+setMethod(
+   f= "naten",
+   signature(mn="MethylSet"),
+   definition=function(mn, fudge=100){
+      object <- mn
+      naten (
+         mn = getMeth(object), 
+         un = getUnmeth(object),
+         fudge
+      ) 
+   }
+)
+
+setMethod(
+   f= "naten",
+   signature(mn="RGChannelSet"),
+   definition=function(mn, fudge=100){
+      mn  <- preprocessRaw(mn)    
+      naten ( mn ) 
+   }
+)
+
+#nanet <- function(mn, un, fudge=100){
+setMethod(
+   f= "nanet",
+   signature(mn="MethylSet"),
+   definition=function(mn, fudge=100){
+      object <- mn
+      nanet (
+         mn = getMeth(object), 
+         un = getUnmeth(object),
+         fudge
+      ) 
+   }
+)
+
+setMethod(
+   f= "nanet",
+   signature(mn="RGChannelSet"),
+   definition=function(mn, fudge=100){
+      mn  <- preprocessRaw(mn)    
+      nanet ( mn ) 
+   }
+)
+
+
+
+#' Internal functions for Illumina i450 normalization functions
+#' 
+#' got and fot find the annotation column differentiating type I and type II
+#' assays in MethylSet (got) or MethyLumiSet (fot) objects. pop extracts
+#' columns from IlluminaHumanMethylation450k.db
+#' 
+#' \code{got} returns a character vector of 'I' and 'II', \code{fot} returns
+#' the index of the relevant column. \code{pop} returns a data frame
+#' 
+#' @aliases got fot pop
+#' @param x a MethyLumiSet
+#' @param obj a MethylSet
+#' @param fd a character vector of the desired annotation columns
+#' @param rn a character vector of the desired features
+#' @author Leonard.Schalkwyk@@kcl.ac.uk
+#' @references Pidsley R, Wong CCY, Volta M, Lunnon K, Mill J, Schalkwyk LC: A
+#' data-driven approach to preprocessing Illumina 450K methylation array data
+#' (submitted)
+#' @export got
+got <- function(obj){
+   I  <- getProbeInfo(obj)$Name
+   rn <- rownames( obj@featureData@data )
+   ot <- rn %in% I 
+   ot[ot] <- 'I'
+   ot[ot=='FALSE'] <- 'II'
+   ot
+}
+
+#nanes <- function(mns, uns, onetwo, fudge=100){
+setMethod(
+   f= "nanes",
+   signature(mns="MethylSet"),
+   definition=function(mns, fudge=100){
+         object <- mns
+         nanes (
+            mns    = getMeth(object), 
+            uns    = getUnmeth(object),
+            onetwo = got(object),
+            fudge
+       ) 
+   }
+)
+
+setMethod(
+   f= "nanes",
+   signature(mn="RGChannelSet"),
+   definition=function(mns, fudge=100){
+      mns  <- preprocessRaw(mns)    
+      nanes ( mns ) 
+   }
+)
+
+#danes <- function(mn, un, onetwo, fudge=100, ...){ 
+setMethod(
+   f= "danes",
+   signature(mn="MethylSet"),
+   definition=function(mn, fudge=100){
+         object <- mn
+         mn <- getMeth(object)
+         danes (
+            mn     , 
+            un     = getUnmeth(object),
+            onetwo = got(object),
+            fudge
+       ) 
+   }
+)
+
+setMethod(
+   f= "danes",
+   signature(mn="RGChannelSet"),
+   definition=function(mn, fudge=100){
+      mn  <- preprocessRaw(mn)    
+      danes ( mn ) 
+   }
+)
+
+#danet <- function(mn, un, onetwo, fudge=100, ...){
+setMethod(
+   f= "danet",
+   signature(mn="MethylSet"),
+   definition=function(mn, fudge=100){
+         object <- mn
+         mn <- getMeth(object) 
+         danet (
+            mn , 
+            un = getUnmeth(object),
+            onetwo = got(object),
+            fudge
+       ) 
+   }
+)
+
+setMethod(
+   f= "danet",
+   signature(mn="RGChannelSet"),
+   definition=function(mn, fudge=100){
+      mn  <- preprocessRaw(mn)    
+      danet ( mn ) 
+   }
+)
+
+
+#daten1 <- function(mn, un, onetwo, fudge=100, ...){
+setMethod(
+   f= "daten1",
+   signature(mn="MethylSet"),
+   definition=function(mn, fudge=100, ...){
+         object <- mn
+         mn <- getMeth(object) 
+         daten1 (
+            mn , 
+            un = getUnmeth(object),
+            onetwo = got(object),
+            fudge, ...
+       ) 
+   }
+)
+
+setMethod(
+   f= "daten1",
+   signature(mn="RGChannelSet"),
+   definition=function(mn, fudge=100, ...){
+      mn  <- preprocessRaw(mn)    
+      daten1 ( mn, ... ) 
+   }
+)
+
+#daten2 <- function(mn, un, onetwo, fudge=100, ...){
+setMethod(
+   f= "daten2",
+   signature(mn="MethylSet"),
+   definition=function(mn, fudge=100, ...){
+         object <- mn
+         mn <- getMeth(object) 
+         daten2 (
+            mn , 
+            un = getUnmeth(object),
+            onetwo = got(object),
+            fudge, ...
+       ) 
+   }
+)
+
+setMethod(
+   f= "daten2",
+   signature(mn="RGChannelSet"),
+   definition=function(mn, fudge=100){
+      mn  <- preprocessRaw(mn)    
+      daten2 ( mn ) 
+   }
+)
+
+
+#ot <- function ( mns, uns, onetwo, fudge=100) {
+setMethod(
+   f= "nasen",
+   signature(mns="MethylSet"),
+   definition=function(mns, fudge=100){
+         object <- mns
+         nasen (
+            mns = getMeth(object), 
+            uns = getUnmeth(object),
+            onetwo = got(object),
+            fudge
+       ) 
+   }
+)
+
+setMethod(
+   f= "nasen",
+   signature(mns="RGChannelSet"),
+   definition=function(mns, fudge=100){
+      mns  <- preprocessRaw(mns)    
+      nasen ( mns ) 
+   }
+)
+
+
+#dasen <- function(mns, uns, onetwo, fudge=100, ...){
+setMethod(
+   f= "dasen",
+   signature(mns="MethylSet"),
+   definition=function(mns, fudge=100){
+      object <- mns
+      dasen (
+         mns = getMeth(object), 
+         uns = getUnmeth(object),
+         onetwo=got(object),
+         fudge
+       ) 
+   }
+)
+
+
+setMethod(
+   f= "dasen",
+   signature(mns="RGChannelSet"),
+   definition=function(mns, fudge=100){
+      mns  <- preprocessRaw(mns)    
+      dasen ( mns ) 
+   }
+)
+
+#danen <- function ( mns, uns, onetwo, fudge=100, ...){
+setMethod(
+   f= "danen",
+   signature(mns="MethylSet"),
+   definition=function(mns, fudge=100, ...){
+         object <- mns
+         danen (
+         mns = getMeth(object), 
+         uns = getUnmeth(object),
+         onetwo=got(object),
+         fudge, ...
+       ) 
+   }
+)
+
+setMethod(
+   f= "danen",
+   signature(mns="RGChannelSet"),
+   definition=function(mns, fudge=100, ...){
+      mns  <- preprocessRaw(mns)    
+      danen ( mns, ... ) 
+   }
+)
+
+
+#tost <- function( mn, un, da, pn ) {  no methylset method because needs detection P values
+setMethod(
+   f= "tost",
+   signature(mn="RGChannelSet"),
+   definition=function(mn){
+      pn <- detectionP(mn)
+      object <- preprocessRaw(mn)
+      mn <- getMeth(object) 
+      tost (
+         mn, 
+         un = getUnmeth(object),
+ #        da = object@featureData@data,  # prob need more columns
+	 da = dfort(featureNames(object)),
+         pn
+      ) 
+   }
+)
+
+#peak.correction <- function (data, anno) {
+setMethod(
+   f= "fuks",
+   signature(data ="MethylSet"),
+   definition=function(data){
+      object <- data
+      fuks (
+         data = getBeta(object), 
+         anno = data.frame(DESIGN=got(object))
+      ) 
+   }
+)
+
+setMethod(
+   f= "fuks",
+   signature(data ="RGChannelSet"),
+   definition=function(data){
+      object <- preprocessRaw(data)
+      fuks (
+         data = getBeta(object), 
+         anno = data.frame(DESIGN=got(object))
+      ) 
+   }
+)
+
+#swan <- function (mn, un, qc ) {
+# swan is already in minfi
+setMethod(
+   f= "swan",
+   signature(mn ="RGChannelSet"),
+   definition=function(mn){
+
+
+   preprocessSWAN(mn)
+
+})
+
+#genki <- function(bn, g=getsnp(rownames(bn)), se=TRUE ){
+setMethod(
+   f= "genki",
+   signature(bn="MethylSet"),
+   definition=function(bn, se=TRUE){
+      object <- bn
+      bn     <- getBeta(object)
+      g      <- getsnp(rownames(bn))
+      genki( bn, g, se ) 
+
+   }
+)
+
+setMethod(
+   f= "genki",
+   signature(bn= "RGChannelSet"),
+   definition=function(bn, se=TRUE){
+#     object <- bn
+#     bn     <- getBeta(object)
+#     g      <- getsnp(rownames(bn))
+#     genki( bn, g, se ) 
+     
+      object <- bn
+      bn     <- getSnpBeta(object)
+      g      <- getsnp(rownames(bn))
+      genki( bn, g, se ) 
+   }
+)
+
+
+#dmrse <- function(betas, idmr=iDMR)
+setMethod(
+   f= "dmrse",
+   signature(betas="MethylSet"),
+   definition=function(betas, idmr=iDMR()){
+      bn     <- getBeta(betas)
+      dmrse( bn, idmr ) 
+     
+   }
+)
+
+setMethod(
+   f= "dmrse",
+   signature(betas= "RGChannelSet"),
+   definition=function(betas, idmr=iDMR()){
+      bn     <- getBeta(betas)
+      dmrse( bn, idmr ) 
+     
+   }
+)
+
+
+
+#dmrse_row <- function(betas, idmr=iDMR)
+setMethod(
+   f= "dmrse_row",
+   signature(betas="MethylSet"),
+   definition=function(betas, idmr=iDMR()){
+      bn     <- getBeta(betas)
+      dmrse_row( bn, idmr ) 
+     
+   }
+)
+
+setMethod(
+   f= "dmrse_row",
+   signature(betas="RGChannelSet"),
+   definition=function(betas, idmr=iDMR()){
+      bn     <- getBeta(betas)
+      dmrse_row( bn, idmr ) 
+     
+   }
+)
+
+
+#dmrse_col <- function(betas, idmr=iDMR)
+setMethod(
+   f= "dmrse_col",
+   signature(betas="MethylSet"),
+   definition=function(betas, idmr=iDMR()){
+      bn     <- getBeta(betas)
+      dmrse_col( bn, idmr ) 
+     
+   }
+)
+
+setMethod(
+   f= "dmrse_col",
+   signature(betas="RGChannelSet"),
+   definition=function(betas, idmr=iDMR()){
+      bn     <- getBeta(betas)
+      dmrse_col( bn, idmr ) 
+     
+   }
+)
+
+#seabi <- function (bn, stop, sex, X){
+setMethod(
+   f= "seabi",
+   signature(bn="MethylSet"),
+   definition=function( bn, stop=1, sex, X ){
+      betas    <- getBeta(bn)
+      seabi( betas, stop, sex, X ) 
+     
+   }
+)
+
+setMethod(
+   f= "seabi",
+   signature(bn="RGChannelSet"),
+   definition=function( bn, stop=1, sex, X ){
+      betas    <- getBeta(bn)
+      seabi( betas, stop, sex, X ) 
+     
+   }
+)
+
+#pfilter<-function(mn, un, bn, da, onetwo, pn, bc, perCount, pnthresh, perc, pthresh){    
+# filter function by Ruth Pidsley
+
+setMethod(
+   f= "pfilter",
+   signature(mn="RGChannelSetExtended"),
+   definition=function(
+      mn=RGChannelSetExtended, 
+      perCount =NULL, pnthresh=NULL, perc=NULL, 
+      pthresh=NULL
+   ){
+      object   <- mn
+      object2  <- preprocessRaw(object)
+#      mn       <- getMeth(object2)
+#      un       <- getUnmeth(object2)
+      pn       <- detectionP(object)
+      bc       <- beadcount(object)
+      l        <- pfilter (
+         mn=NULL, un=NULL, bn=NULL, 
+         da=NULL, 
+         pn=pn, bc=bc,
+         perCount, 
+	 pnthresh,perc,pthresh,
+         logical.return= TRUE
+      )
+#     object3 <- object[l$probes,] 
+      object3 <- object2[,l$samples] 
+
+#      output <-list("RGobject" = object3,
+#                    "Filter" = l$probes)
+
+    fil <- assayDataNew(Meth = assayDataElement(object3,"Meth"), 
+                        Unmeth = assayDataElement(object3,"Unmeth"),
+                        Filter=matrix(rep(l$probes, length(l$sample)), nrow=length(l$probes), ncol=length(l$sample))
+                       )
+    newObject <- new("MethylSet", assayData=fil)
+    newObject@annotation <- annotation(object3)
+    newObject@phenoData <- phenoData(object3)
+    return(newObject)
+    }
+)
+
+#BMIQ <- function(beta.v,design.v,nL=3,doH=TRUE,nfit=50000,th1.v=c(0.2,0.75),th2.v=NULL,niter=5,tol=0.001,plots=TRUE,sampleID=1){
+setMethod(
+   f= "BMIQ",
+   signature(beta.v="MethylSet"),
+   definition=function(
+		 beta.v,
+      nL=3, doH=TRUE, nfit=5000,
+      th1.v=c(0.2,0.75), th2.v=NULL, 
+      niter=5, tol=0.001, plots=FALSE, 
+      pri=FALSE       
+		       
+		       ){
+      object <- beta.v
+      d <- as.numeric(factor(got(object)))
+      ibetas <- getBeta(object) 
+      betas <- sapply ( 
+         colnames(ibetas), 
+         function(name){
+            ou <- try(
+               BMIQ(
+                  ibetas[,name],
+                  design.v=d, nL, doH, 
+                  nfit, th1.v, th2.v, 
+                  niter, tol, plots, 
+                  sampleID=name, 
+                  pri=FALSE 
+               )
+            )
+            if(inherits(ou, 'try-error')){
+               ou <- rep(NA,dim(ibetas)[1])
+               warn(paste(name, "failed!"))
+            }else{
+               ou <- ou$nbeta
+            }
+          }
+       )
+   }
+)
+# bscon <- function(x, ...) { UseMethod (bscon, x ) }
+# see also bscon_methy and bscon_minfi
+
+setMethod(
+   f= "bscon",
+   signature(x="RGChannelSet"),
+   definition=function( x ){
+      bscon_minfi(x)
+   }
+)
+
+# outlyx <- function(x, y, dist1, dist2) 
+setMethod(
+   f= "outlyx",
+   signature(x="RGChannelSet"),
+   definition=function(x, iqr, iqrP, pc, mv, mvP, plot){
+   x <- getBeta(x)
+   outlyx(x, iqr, iqrP, pc, mv, mvP, plot)
+   }
+)
+
+setMethod(
+   f= "outlyx",
+   signature(x="MethylSet"),
+   definition=function(x, iqr, iqrP, pc, mv, mvP, plot){
+   x <- getBeta(x)
+   outlyx(x, iqr, iqrP, pc, mv, mvP, plot)
+   }
+)
+
+setMethod(
+   f= "pwod",
+   signature(object="RGChannelSet"), 
+   definition=function(object, mul){
+   object <- getBeta(object)
+   pwod(object, mul)
+   }
+)
+
+setMethod(
+   f= "pwod",
+   signature(object="MethylSet"), 
+   definition=function(object, mul){
+   object <- getBeta(object)
+   pwod(object, mul)
+   }
+)
+
+
+
